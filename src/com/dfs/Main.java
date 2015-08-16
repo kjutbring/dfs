1db132a17f5a37ea2bb95be54228c7d15c01f7a0
kjutbring <kittyj@cmail.nu>
2015-08-16 19:43:44 +0200

    generate maze

--- src/com/dfs/Main.java
+++ src/com/dfs/Main.java
@@ -24,16 +24,9 @@ public class Main {
         int exitX = 0;
         int exitY = 0;
 
-        mazeDfs(startX, startY, grid);
+        grid = mazeDfs(startX, startY, grid);
 
-        ArrayList<ArrayList<MazeNode>> gri = mazeDfs(4,2,grid);
-
-        System.out.println(gri.get(2).get(4).isConnectedDown());
-        System.out.println(gri.get(2).get(4).isConnectedUp());
-        System.out.println(gri.get(2).get(4).isConnectedLeft());
-        System.out.println(gri.get(2).get(4).isConnectedRight());
-
-
+        System.out.println("Done...?");
 
     }
 
@@ -41,66 +34,68 @@ public class Main {
     /*
         This method is recursive and visits each node until the exit.
      */
-    public static ArrayList<ArrayList<MazeNode>> mazeDfs(int x, int y, ArrayList<ArrayList<MazeNode>> mazeGrid) {
-        // initialize stack
-        Stack<MazeNode> stack = new Stack();
-        ArrayList<ArrayList<MazeNode>> grid = mazeGrid;
+    public static ArrayList<ArrayList<MazeNode>> mazeDfs(int x, int y, ArrayList<ArrayList<MazeNode>> grid) {
+        Stack stack = new Stack();
 
-        // set active node and mark as visited
+        MazeNode tempNode;
         MazeNode node = grid.get(y).get(x);
-        node.setVisited(true);
 
-        // add node to stack
-        stack.push(node);
 
-        // check if node have any unvisited neighbours
-        if (!MazeNode.determineVisitedNeighbours(node)) {
 
-            if (randomDirections(node) == 0) {
-                // check if position is outside grid
-                if (y - 1 >= 0) {
-                    node.setConnectedUp(true);
-                    grid.get(y - 1).get(x).setConnectedDown(true);
-                    mazeDfs(x, y - 1, grid);
+        while (!node.isVisited()) {
+            node.setVisited(true);
+            // check if node have any unvisited neighbours
+            if (!MazeNode.determineVisitedNeighbours(node)) {
+                stack.push(node);
+
+                if (randomDirections(node) == 0) {
+                    // check if position is outside grid
+                    if (y - 1 >= 0) {
+                        node.setConnectedUp(true);
+                        grid.get(y - 1).get(x).setConnectedDown(true);
+                        node = grid.get(y - 1).get(x);
+                    }
                 }
-            }
 
-            if (randomDirections(node) == 1) {
-                // check if position is outside grid.
-                if (y + 1 <= 9) {
-                    node.setConnectedDown(true);
-                    grid.get(y + 1).get(x).setConnectedUp(true);
-                    mazeDfs(x, y + 1, grid);
+                if (randomDirections(node) == 1) {
+                    // check if position is outside grid.
+                    if (y + 1 <= 9) {
+                        node.setConnectedDown(true);
+                        grid.get(y + 1).get(x).setConnectedUp(true);
+                        node = grid.get(y + 1).get(x);
+                    }
                 }
-            }
 
-            if (randomDirections(node) == 2) {
-                // check if position is outside grid
-                if (x - 1 >= 0) {
-                    node.setConnectedLeft(true);
-                    grid.get(y).get(x - 1).setConnectedRight(true);
-                    mazeDfs(x - 1, y, grid);
+                if (randomDirections(node) == 2) {
+                    // check if position is outside grid
+                    if (x - 1 >= 0) {
+                        node.setConnectedLeft(true);
+                        grid.get(y).get(x - 1).setConnectedRight(true);
+                        node = grid.get(y).get(x - 1);
+                    }
                 }
-            }
 
-            if (randomDirections(node) == 3) {
-                // check if position is outside grid
-                if (x + 1 <=9) {
-                    node.setConnectedRight(true);
-                    grid.get(y).get(x + 1).setConnectedLeft(true);
-                    mazeDfs(x + 1, y, grid);
+                if (randomDirections(node) == 3) {
+                    // check if position is outside grid
+                    if (x + 1 <= 9) {
+                        node.setConnectedRight(true);
+                        grid.get(y).get(x + 1).setConnectedLeft(true);
+                        node = grid.get(y).get(x + 1);
+
+                    }
                 }
-            }
-
-        } /* else {
-            if (!stack.empty()) {
-                node = stack.peek();
-                mazeDfs(node.getPx(), node.getPy(), grid);
+            } else {
+                if (!stack.empty()) {
+                    tempNode = (MazeNode)stack.pop();
+                    node = grid.get(tempNode.getPy()).get(tempNode.getPx());
+                }
             }
         }
-        */
         return grid;
+
     }
+
+
 
 
     /*
